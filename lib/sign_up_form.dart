f6e36e28217a0069329d33a3c7473f12025e86b7
whitejm <emailwhitej@gmail.com>
2024-04-25 21:21:10 -0700

    signup form and remove unneeded widget

--- lib/sign_up_form.dart
+++ lib/sign_up_form.dart
@@ -22,76 +22,89 @@ class _SignUpFormState extends State<SignUpForm> {
   Widget build(BuildContext context) {
     return Consumer<PocketBaseAuthNotifier>(
       builder: (context, auth, child) {
-        return Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Form(
+                key: _formKey,
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 600),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: _emailController,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                          ),
+                          validator: (value) =>
+                              value!.isEmpty ? 'Please enter your email' : null,
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          controller: _passwordController,
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                          ),
+                          obscureText: true,
+                          validator: (value) => value!.isEmpty
+                              ? 'Please enter your password'
+                              : null,
+                        ),
+                        TextFormField(
+                          controller: _passwordConfirmController,
+                          decoration: const InputDecoration(
+                            labelText: 'Confirm Password',
+                          ),
+                          obscureText: true,
+                          validator: (value) => value!.isEmpty
+                              ? 'Please enter your password'
+                              : null,
+                        ),
+                        const SizedBox(height: 20.0),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromARGB(255, 158, 183, 58),
+                          ),
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              try {
+                                await auth.signUp(
+                                  _emailController.text,
+                                  _passwordController.text,
+                                  _passwordController.text,
+                                );
+                                Navigator.popAndPushNamed(context, '/');
+                              } catch (error) {
+                                print(auth.errorMessage);
+                                print(error);
+                              }
+                              // Handle successful login or error depending on _authNotifier state
+                            }
+                          },
+                          child:
+                              Text(auth!.isLoading ? 'Loading...' : 'Sign Up'),
+                        ),
+                        const SizedBox(height: 10),
+                        InkWell(
+                          onTap: () {
+                            Navigator.popAndPushNamed(context, '/login');
+                          },
+                          child: Text('Login'),
+                        ),
+                        if (auth.errorOccurred)
+                          Text(
+                            auth.errorMessage,
+                            style: const TextStyle(color: Colors.red),
+                          ),
+                      ],
+                    ),
                   ),
-                  validator: (value) =>
-                      value!.isEmpty ? 'Please enter your email' : null,
                 ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                  ),
-                  obscureText: true,
-                  validator: (value) =>
-                      value!.isEmpty ? 'Please enter your password' : null,
-                ),
-                TextFormField(
-                  controller: _passwordConfirmController,
-                  decoration: const InputDecoration(
-                    labelText: 'Confirm Password',
-                  ),
-                  obscureText: true,
-                  validator: (value) =>
-                      value!.isEmpty ? 'Please enter your password' : null,
-                ),
-                const SizedBox(height: 20.0),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 158, 183, 58),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      try {
-                        await auth.signUp(
-                          _emailController.text,
-                          _passwordController.text,
-                          _passwordController.text,
-                        );
-                        Navigator.popAndPushNamed(context, '/');
-                      } catch (error) {
-                        print(auth.errorMessage);
-                        print(error);
-                      }
-                      // Handle successful login or error depending on _authNotifier state
-                    }
-                  },
-                  child: Text(auth!.isLoading ? 'Loading...' : 'Sign Up'),
-                ),
-                const SizedBox(height: 10),
-                InkWell(
-                  onTap: () {
-                    Navigator.popAndPushNamed(context, '/login');
-                  },
-                  child: Text('Login'),
-                ),
-                if (auth.errorOccurred)
-                  Text(
-                    auth.errorMessage,
-                    style: const TextStyle(color: Colors.red),
-                  ),
-              ],
-            ),
+              ),
+            ],
           ),
         );
       },
