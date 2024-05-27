c6c5c16046c8108d8d533b3ff8f6df5b1de06465
whitejm <emailwhitej@gmail.com>
2024-05-27 15:21:52 -0700

    take 3

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,21 @@ import 'package:sn/pocketbase_library.dart';
 import 'login_form.dart';
 import 'password_reset_form.dart';
 import 'sign_up_form.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:pocketbase/pocketbase.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: ".env");
+  // Load environment variables from Netlify if available
+  String pbUrl = String.fromEnvironment('POCKETBASE');
 
-  String pbUrl = dotenv.get('POCKETBASE', fallback: 'http://127.0.0.1:8090');
+  // If the environment variable is not available in Netlify, load from .env file
+  if (pbUrl.isEmpty) {
+    await dotenv.load();
+    pbUrl = dotenv.get('POCKETBASE', fallback: 'http://127.0.0.1:8090');
+  }
+
   final pb = PocketBase(pbUrl);
 
   runApp(MultiProvider(
